d0f902bd4d192ca73d06bfd3b6467b80a8b1875c
VlasTru <32749309+VlasTru@users.noreply.github.com>
2026-01-20 12:18:44 +0400

    Update flights.R

--- flights.R
+++ flights.R
@@ -1,15 +1,30 @@
+library(shiny)
 library(httr2)
 library(dplyr)
 library(DBI)
 library(duckdb)
-resp <- request("https://api.travelpayouts.com/aviasales/v3/get_latest_prices?origin=EVN&destination=IST&currency=USD&departure_at=2025-01&token=3b524593b528496317c30f34007c4265")
-resp %>% req_dry_run()
-json <- req_perform(resp)
-resp_body_string(json)
-data_to_load <- json %>% resp_body_json(simplifyVector = TRUE) %>% as_tibble()
-data_to_load
-con <- dbConnect(duckdb(), dbdir = "flights.duckdb")
-dbWriteTable(con, "flights", data_to_load, append = TRUE)
-res <- dbGetQuery(con, "SELECT data.depart_date, data.value FROM flights order by data.value asc limit 10")
-print(res)
-dbDisconnect(con)
+library(DT)
+ui <- fluidPage(
+    titlePanel("Flights yo"),
+    DTOutput("table")
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+
+  
+      resp <- request("https://api.travelpayouts.com/aviasales/v3/get_latest_prices?origin=EVN&destination=IST&currency=USD&departure_at=2025-01&token=3b524593b528496317c30f34007c4265")
+      json <- req_perform(resp)
+      resp_body_string(json)
+      data_to_load <- json %>% resp_body_json(simplifyVector = TRUE) %>% as_tibble()
+      data_to_load
+      con <- dbConnect(duckdb(), dbdir = "flights.duckdb")
+      dbWriteTable(con, "flights", data_to_load, append = TRUE)
+      res <- dbGetQuery(con, "SELECT data.value FROM flights")
+      output$table <- renderDT(res)
+      dbDisconnect(con)
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
